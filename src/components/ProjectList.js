0792ba3a75b069f0c7e093026ed5454ea0e74230
Gonzodamus <Dick@TheDickWard.com>
2018-02-06 21:21:12 -0500

    cards for projects finished.  Might not be the best approach if I'm using modals to display projects though.  Let's look at this in the morning

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,21 +1,108 @@
 import React from "react";
-import { Row, Col, Card } from "reactstrap";
+import {
+  Row,
+  Col,
+  Card,
+  CardTitle,
+  CardSubtitle,
+  CardBody,
+  CardLink,
+  CardText,
+  CardImg
+} from "reactstrap";
 
 const ProjectList = () => {
   return (
-    <Row>
-      <Col
-        style={{ margin: "auto" }}
-        xs="12"
-        sm={{ size: "5", offset: 1 }}
-        lg={{ size: "4", offset: 2 }}
-      >
-        <Card>hi</Card>
-      </Col>
-      <Col style={{ margin: "auto" }} xs="12" sm="5" lg="4">
-        <Card>hi</Card>
-      </Col>
-    </Row>
+    <div>
+      <Row>
+        <Col
+          className="projectCard"
+          xs="12"
+          sm={{ size: "5", offset: 1 }}
+          lg={{ size: "4", offset: 2 }}
+        >
+          <Card style={{ maxWidth: "250px", margin: "auto" }}>
+            <CardBody>
+              <CardTitle className="text-center">DonorPool</CardTitle>
+            </CardBody>
+            <CardSubtitle className="text-center">
+              Ruby | Rails | React | Redux
+            </CardSubtitle>
+            <img
+              className="projectImage"
+              src="http://via.placeholder.com/150x150"
+              alt="Placeholder"
+            />
+            <CardBody>
+              <a href="#">More Info</a> |
+              <a href="#">Github</a> |
+              <a href="#">Live Demo</a>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col className="projectCard" xs="12" sm="5" lg="4">
+          <Card style={{ maxWidth: "250px", margin: "auto" }}>
+            <CardBody>
+              <CardTitle className="text-center">Schedulr</CardTitle>
+            </CardBody>
+            <CardSubtitle className="text-center">Card subtitle</CardSubtitle>
+            <img
+              className="projectImage"
+              src="http://via.placeholder.com/150x150"
+              alt="Placeholder"
+            />
+            <CardBody>
+              <a href="#">More Info</a> |
+              <a href="#">Github</a> |
+              <a href="#">Live Demo</a>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col
+          className="projectCard"
+          xs="12"
+          sm={{ size: "5", offset: 1 }}
+          lg={{ size: "4", offset: 2 }}
+        >
+          <Card style={{ maxWidth: "250px", margin: "auto" }}>
+            <CardBody>
+              <CardTitle className="text-center">Peeker</CardTitle>
+            </CardBody>
+            <CardSubtitle className="text-center">Card subtitle</CardSubtitle>
+            <img
+              className="projectImage"
+              src="http://via.placeholder.com/150x150"
+              alt="Placeholder"
+            />
+            <CardBody>
+              <a href="#">More Info</a> |
+              <a href="#">Github</a> |
+              <a href="#">Live Demo</a>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col className="projectCard" xs="12" sm="5" lg="4">
+          <Card style={{ maxWidth: "250px", margin: "auto" }}>
+            <CardBody>
+              <CardTitle className="text-center">H-I-I</CardTitle>
+            </CardBody>
+            <CardSubtitle className="text-center">Card subtitle</CardSubtitle>
+            <img
+              className="projectImage"
+              src="http://via.placeholder.com/150x150"
+              alt="Placeholder"
+            />
+            <CardBody>
+              <a href="#">More Info</a> |
+              <a href="#"> Github</a> |
+              <a href="#"> Live Demo</a>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </div>
   );
 };
 
--- src/components/Splash.js
+++ src/components/Splash.js
@@ -3,67 +3,65 @@ import { Row, Col, Button } from "reactstrap";
 
 const Splash = () => {
   return (
-    <div>
-      <Row style={{ minHeight: "800px" }}>
-        <Col
-          className="splashColumn"
-          xs="4"
-          sm={{ size: "5", offset: 1 }}
-          lg={{ size: "4", offset: 2 }}
-        >
-          <div className="splashButtonBox">
-            <Button
-              outline
-              color="info"
-              style={{ display: "block" }}
-              className="splashButton"
-            >
-              Projects
-            </Button>
+    <Row style={{ minHeight: "800px" }}>
+      <Col
+        className="splashColumn"
+        xs="4"
+        sm={{ size: "5", offset: 1 }}
+        lg={{ size: "4", offset: 2 }}
+      >
+        <div className="splashButtonBox">
+          <Button
+            outline
+            color="info"
+            style={{ display: "block" }}
+            className="splashButton"
+          >
+            Projects
+          </Button>
 
-            <Button
-              outline
-              color="info"
-              style={{ display: "block" }}
-              className="splashButton"
-            >
-              Resume
-            </Button>
-            <Button
-              outline
-              color="info"
-              style={{ display: "block" }}
-              className="splashButton"
-            >
-              Github
-            </Button>
-            <Button
-              outline
-              color="info"
-              style={{ display: "block" }}
-              className="splashButton"
-            >
-              Blog
-            </Button>
-          </div>
-        </Col>
-        <Col xs="8" sm="5" lg="4" className="splashColumn">
-          <Row>
-            <Col>
-              <div className="divOutline socialBar">
-                <i className="fab fa-twitter-square fa-lg splashIcon" />
-                <i className="fab fa-linkedin fa-lg splashIcon" />
-                TheDickWard@gmail.com
-              </div>
-            </Col>
-          </Row>
-          <div className="splashTextBox">
-            <h1 className="divOutline splashHeadline">Dick Ward</h1>
-            <p className="divOutline splashByline">Full-Stack Web Developer</p>
-          </div>
-        </Col>
-      </Row>
-    </div>
+          <Button
+            outline
+            color="info"
+            style={{ display: "block" }}
+            className="splashButton"
+          >
+            Resume
+          </Button>
+          <Button
+            outline
+            color="info"
+            style={{ display: "block" }}
+            className="splashButton"
+          >
+            Github
+          </Button>
+          <Button
+            outline
+            color="info"
+            style={{ display: "block" }}
+            className="splashButton"
+          >
+            Blog
+          </Button>
+        </div>
+      </Col>
+      <Col xs="8" sm="5" lg="4" className="splashColumn">
+        <Row>
+          <Col>
+            <div className="divOutline socialBar">
+              <i className="fab fa-twitter-square fa-lg splashIcon" />
+              <i className="fab fa-linkedin fa-lg splashIcon" />
+              TheDickWard@gmail.com
+            </div>
+          </Col>
+        </Row>
+        <div className="splashTextBox">
+          <h1 className="divOutline splashHeadline">Dick Ward</h1>
+          <p className="divOutline splashByline">Full-Stack Web Developer</p>
+        </div>
+      </Col>
+    </Row>
   );
 };
 
