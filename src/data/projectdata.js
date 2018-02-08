a9cab16ad3fdbdad53d1a670c7c4e871d6f8befb
Gonzodamus <Dick@TheDickWard.com>
2018-02-08 09:02:03 -0500

    began creating data file for project info

---
+++ src/data/projectdata.js
@@ -0,0 +1,43 @@
+export const projects = [
+  {
+    name: "DonorPool",
+    description: "A social network based around charitable giving.",
+    tech: "React, Redux, JavaScript, Rails, Ruby, Bootstrap, AWS",
+    repoFront: "https://github.com/Gonzodamus/DonorPool-client",
+    repoBack: "https://github.com/Gonzodamus/backend-DonorPool",
+    repoFull: "",
+    liveDemo: "https://powerful-savannah-20154.herokuapp.com/",
+    image: "http://via.placeholder.com/200x200"
+  },
+  {
+    name: "Schedulr",
+    description: "The day scheduling app built around you, not the calendar.",
+    tech: "React, React Draggable, JavaScript, Rails, Ruby, Semantic UI",
+    repoFront: "https://github.com/Gonzodamus/schedulr-client",
+    repoBack: "https://github.com/Gonzodamus/schedulr-backend",
+    repoFull: "",
+    liveDemo: "https://dashboard.heroku.com/apps/fathomless-forest-91982",
+    image: "http://via.placeholder.com/200x200"
+  },
+  {
+    name: "Peeker",
+    description:
+      "Rails based dating app using Clarifai's photo analysis to create matches.",
+    tech: "Rails, Ruby, Clarifai API, JSON",
+    repoFront: "",
+    repoBack: "",
+    repoFull: "",
+    liveDemo: "",
+    image: "http://via.placeholder.com/200x200"
+  },
+  {
+    name: "",
+    description: "",
+    tech: "",
+    repoFront: "",
+    repoBack: "",
+    repoFull: "",
+    liveDemo: "",
+    image: "http://via.placeholder.com/200x200"
+  }
+];
