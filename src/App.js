9e90bcc70814bfa499291f1b0abb4422a9cf2c39
Gonzodamus <Dick@TheDickWard.com>
2018-02-10 12:02:06 -0500

    arrow now has visibility toggle

--- src/App.js
+++ src/App.js
@@ -4,17 +4,34 @@ import Bio from "./components/Bio";
 import CurrentProject from "./components/CurrentProject";
 import ProjectList from "./components/ProjectList";
 import PortfolioFooter from "./components/PortfolioFooter";
-import { Row, Col } from "reactstrap";
+import { Row, Col, Button } from "reactstrap";
 
 class App extends Component {
+  state = {
+    arrowVisibility: false
+  };
   render() {
+    window.onscroll = () => {
+      if (window.pageYOffset > 200) {
+        this.setState({ arrowVisibility: true });
+      } else {
+        this.setState({ arrowVisibility: false });
+      }
+    };
+
     return (
       <div
+        id="top"
         style={{
           maxWidth: "1500px",
           margin: "auto"
         }}
       >
+        {this.state.arrowVisibility ? (
+          <Button href="#top" outline color="info" className="buttonUp">
+            <i className="fas fa-angle-double-up fa-3x" />
+          </Button>
+        ) : null}
         <Row>
           <Col style={{ margin: "auto" }} xs="12" sm="10" lg="8">
             <Splash />
