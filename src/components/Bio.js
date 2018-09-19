472ffaaedf386f45a2c95a4ef85e1f7c9b43501f
gonzodamus <github email address>
2018-09-19 10:42:53 -0400

    updated bio and current project text

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -4,13 +4,8 @@ const Bio = () => {
   return (
     <div id="bio" className="textBox" style={{ paddingTop: "20px" }}>
       <hr className="bioLine" />
-      Full stack web developer with a flair for the theatrical and a mission to
-      leave the world a better place than I got it. Experienced in Rails and
-      Javascript programming including React and Redux. I discovered web
-      development while customizing a professional website for myself, and got
-      hooked on the problem solving and creativity of programming. My background
-      in improvisation helps me to bring a team-based work ethic to anything I
-      do.
+      Full stack web developer with a flair for the theatrical and a mission to leave the world a better place than I got it. Experienced in Javascript programming including React and Redux, as well as Ruby on Rails. I discovered web development while customizing a professional website for myself, and got hooked on the problem solving and creativity of programming. After graduating Flatiron School, I was hired on as a Technical Coaching Fellow to help teach new students, write curriculum, and further my knowledge of the subjects we teach.
+
     </div>
   );
 };
--- src/components/CurrentProject.js
+++ src/components/CurrentProject.js
@@ -5,12 +5,22 @@ const CurrentProject = () => {
     <div>
       <hr className="currentProjectsLine" />
       <div className="textBox">
-        After building out 'DonorPool,' my first app using Redux, I took some
-        time to build out a portfolio page, which you're on now. Hi! I've also
-        been reworking my resume and going back to make a few of my older
-        projects a bit more friendly code-wise. Next up, I'll be looking into
-        setting up servers for my web apps, rather than hosting them on Heroku
-        or AWS.
+        <p>
+          I just finished launching <a href="https://medium.com/@TheDickWard/creating-a-fabulous-frontend-for-flatiron-field-day-9d0b09a6a652">Flatiron Field Day</a>, which was an all-day mini hackathon for our web development students.
+          My 15-person team and I were able to create a stable project that was met with high praise by all the
+          students that participated.
+        </p>
+
+
+        <p>
+          My next big push is going to be a deeper dive into React, and completion of a personal project of mine
+          which uses React, Redux, and data-set that's going to force me to make some interesting design decisions.
+        </p>
+
+        <p>
+        I'm also working my way through 'The Complete Junior to Senior Developer' on Udemy, which I'm finding fasctinating!
+        Writing effective tests is something I've long been interested in, and I'm looking forward to having that skillset.
+        </p>
       </div>
     </div>
   );
